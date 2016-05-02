aed98c11e0d48eb8c11f815153b157ee53e8e72e
matthewpateman <matthewpateman@me.com>
2016-05-02 20:01:36 +0100

    APP POST

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ app.get('/webhook/', function (req, res) {
 
 // to post data
 app.post('/webhook/', function (req, res) {
+	console.log("APP POST");
 	messaging_events = req.body.entry[0].messaging
 	for (i = 0; i < messaging_events.length; i++) {
 		event = req.body.entry[0].messaging[i]
